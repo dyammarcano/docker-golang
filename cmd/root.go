6ef41efbbc66d0c3ad902b77021b294c9a052e10
Dyam Marcano <dyam.marcano@gmail.com>
2024-04-02 23:50:21 -0300

    first commit

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"os"
-
 	"github.com/spf13/cobra"
 )
 
@@ -24,7 +22,5 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		os.Exit(1)
-	}
+	cobra.CheckErr(rootCmd.Execute())
 }
